90052c6652106c1560cfbe00d56932c139d68409
TMHoang01 <tmhoang2601@gmail.com>
2022-08-19 14:13:49 +0700

    create postLink

--- templates/Admin/Users/index.php
+++ templates/Admin/Users/index.php
@@ -19,9 +19,12 @@
 
 
     <div class="table-responsive">
+        <?= $this->Form->create(null,['url' =>['action' => 'deleteAll'] ]) ?>
+        <button>Deletel All</button>
         <table>
             <thead>
                 <tr>
+                    <th>#</th>
                     <th><?= $this->Paginator->sort('id') ?></th>
                     <th><?= $this->Paginator->sort('username') ?></th>
                     <th><?= $this->Paginator->sort('email') ?></th>
@@ -36,6 +39,8 @@
             <tbody>
                 <?php foreach ($users as $user): ?>
                 <tr>
+                    <td><?= $this->Form->checkbox('ids[]',['value' => $user->id]) ?> </td>
+
                     <td><?= $this->Number->format($user->id) ?></td>
                     <td><?= h($user->username) ?></td>
                     <td><?= h($user->email) ?></td>
@@ -47,13 +52,16 @@
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $user->id]) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id)]) ?>
+                        <?= $this->Form->postLink(__('Delete'),  ['action' => 'delete', $user->id], ['block'=>true, 'confirm' => __(' Are you sure you want to delete user # {0}?', $user->id)]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
+        <?= $this->Form->end(); ?>
+        <?= $this->fetch('postLink'); ?>
     </div>
+
     <div class="paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
